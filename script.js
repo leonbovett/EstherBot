6ad624bdde360e8798dc2b71ff6e45ed3e1bf2cd
Leon Bovett <leon.bovett@accenture.com>
2016-04-26 20:42:49 +0100

    adding states

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about EX4U? Just say HELLO to get started.')
+            return bot.say('Hello Shaun, how can I help you today?')
                 .then(() => 'speak');
         }
     },
